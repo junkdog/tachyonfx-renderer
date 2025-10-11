4c1f82a9f4e9b14cc1c1b6eb36822d3816e7b4fe
Adrian Papari <adrian.papari@assaabloy.com>
2025-10-11 12:36:06 +0200

    add rustfmt, more wireing

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,6 @@
-use tachyonfx::{Duration, EffectManager};
+use ratatui::buffer::Buffer;
+use ratatui::layout::Rect;
+use tachyonfx::{Duration, Effect, EffectManager};
 use crate::event::AppEvent;
 
 pub struct App {
@@ -17,6 +19,33 @@ impl App {
             last_tick_instant: web_time::Instant::now(),
             last_tick_duration: Duration::default(),
             is_running: true,
+        }
+    }
+
+    pub fn tick(&mut self) -> Duration {
+        let now = web_time::Instant::now();
+        let elapsed = now.duration_since(self.last_tick_instant).as_millis();
+
+        self.last_tick_instant = now;
+        self.last_tick_duration = Duration::from_millis(elapsed as u32);
+
+        self.last_tick_duration
+    }
+
+    pub fn process_effects(&mut self, last_frame_duration: Duration, buf: &mut Buffer, area: Rect) {
+        self.effects.process_effects(last_frame_duration, buf, area);
+    }
+
+    pub fn register_effect(&mut self, effect: Effect) {
+        self.effects.add_effect(effect);
+    }
+
+    pub fn apply_event(&mut self, event: AppEvent) {
+        match event {
+            AppEvent::Tick => (),
+            AppEvent::Resize(w, h) => {}
+            AppEvent::ReplaceCanvas(ansi) => {}
+            AppEvent::CompileDsl(code) => {}
         }
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use ratatui::Terminal;
+use ratatui::{Frame, Terminal};
 use ratzilla::backend::webgl2::WebGl2BackendOptions;
 use ratzilla::{WebGl2Backend, WebRenderer};
 use eyre::Result;
+use crate::event_handler::EventHandler;
 
 mod app;
 mod dispatcher;
@@ -12,24 +13,40 @@ mod theme;
 
 fn main() -> Result<()> {
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+    let events = EventHandler::new(core::time::Duration::from_millis(33));
+
     let options = WebGl2BackendOptions::new()
-            .measure_performance(true)
+            .enable_console_debug_api()
+            .enable_mouse_selection()
             .grid_id("container")
-            .enable_console_debug_api();
+            .measure_performance(true);
 
     let backend = WebGl2Backend::new_with_options(options)
         .map_err(|e| eyre::eyre!("{:?}", e))?;
     let terminal = Terminal::new(backend)
         .map_err(|e| eyre::eyre!("{:?}", e))?;
 
-    let mut last_tick = web_time::Instant::now();
+
+    let mut app = app::App::new(events.sender());
 
     terminal.draw_web(move |frame| {
-        let now = web_time::Instant::now();
-        let elapsed = now.duration_since(last_tick);
-        last_tick = now;
+        while let Some(event) = events.try_next() {
+            app.apply_event(event);
+        }
 
-        // frame.render_effect(&mut effect, frame.area(), elapsed.into());
+        let elapsed = app.tick();
+        let area = frame.area();
+
+        render_ui(frame);
+        app.process_effects(elapsed, frame.buffer_mut(), area);
+
     });
+
     Ok(())
+}
+
+fn render_ui(
+    f: &mut Frame<'_>,
+) {
+    let screen = f.area().as_size();
 }
